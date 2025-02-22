539486c4630d79d8d4d6b10ce80c233d272a4135
Anya Harris <aunnie@MacBookAir.home.local>
2025-02-21 20:21:13 -0500

    commit changes

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const jokeEl = document.getElementById('joke');
 const jokeBtn = document.getElementById('jokeBtn');
 
-function generateJoke() {
+async function generateJoke() {
    const config = {
      headers: {
        Accept: 'application/json',
      },
    };
-
-   fetch('https://icanhazdadjoke.com', config)
-     .then((res) => res.json())
-     .then((data) => {
-       console.log(data);
-     });
- }
+  
+    const res = await fetch('https://icanhazdadjoke.com', config);
+    const data = await res.json();
+    jokeEl.textContent = data.joke;
+  }
 
 generateJoke();
+
+jokeBtn.addEventListener('click', generateJoke);
+
